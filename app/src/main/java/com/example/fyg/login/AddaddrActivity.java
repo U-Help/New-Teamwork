6c4f1636a4045924889067f17d49d6da6b86f8d9
qth <qth@mail.ustc.edu.cn>
2018-07-19 11:27:49 +0800

    qth

---
+++ app/src/main/java/com/example/fyg/login/AddaddrActivity.java
@@ -0,0 +1,13 @@
+package com.example.fyg.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddaddrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addaddr);
+    }
+}
