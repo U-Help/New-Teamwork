7648b538a85022ee0d76b4c75f52f9cad931c482
qth <qth@mail.ustc.edu.cn>
2018-07-22 21:40:52 +0800

    qth

--- app/src/main/java/com/example/fyg/login/SwipeActivity.java
+++ app/src/main/java/com/example/fyg/login/SwipeActivity.java
@@ -1,74 +1,66 @@
 package com.example.fyg.login;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class SwipeActivity extends AppCompatActivity /*implements  SwipeRefreshLayout.OnRefreshListener*/{
-    private SwipeRefreshLayout swiper;
-    private ListView mListview;
+public class SwipeActivity extends Activity implements SwipeRefreshLayout.OnRefreshListener {
+    private SwipeRefreshLayout mSwipeLayout;
+    private ListView mListView;
+    private ArrayList<String> list = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
 
-    private List<String> list = new ArrayList<>();
-
-    private ArrayAdapter adapter;
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_swiperefresh);
+        setContentView(R.layout.activity_main);
 
-        /*swiper = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        mListView = (ListView) findViewById(R.id.listview);
+        /**
+         * listview绑定adapter
+         */
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getData());
+        mListView.setAdapter(adapter);
 
-        swiper.setOnRefreshListener(this);
-
-        swiper.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        //绑定刷新时间
+        mSwipeLayout.setOnRefreshListener(this);
+        //设置颜色
+        mSwipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light, android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
-
-        mListview = (ListView) findViewById(R.id.list_view);
-
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getData());
-
-        mListview.setAdapter(adapter);*/
     }
-   /* private List<String> getData(){
-        list.add("hello");
-        list.add("this is a beautiful world");
-        list.add("an android app is used by you");
-        list.add("its name is 'U-Help'");
+
+    private ArrayList<String> getData() {
+        list.add("Hello world!");
+        list.add("CSDN:程序员小冰");
+        list.add("An Android Developer");
+        list.add("http://weibo.com/mcxiaobing");
+        list.add("http://git.oschina.net/MCXIAOBING");
+        list.add("https://github.com/QQ986945193");
+        list.add("An Android Developer");
+        list.add("http://weibo.com/mcxiaobing");
+        list.add("http://git.oschina.net/MCXIAOBING");
+        list.add("https://github.com/QQ986945193");
+        list.add("An Android Developer");
+        list.add("http://weibo.com/mcxiaobing");
+        list.add("http://git.oschina.net/MCXIAOBING");
+        list.add("https://github.com/QQ986945193");
         return list;
     }
 
-    @Override
-    public void onRefresh(){
-        //刷新
+    public void onRefresh() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                //结束后停止刷新
-                swiper.setRefreshing(false);
-            }
-        },3000);
-        //一般从后端获取数据
-        //  new Handler().post(new Runnable(){
-        //      @Override
-        //      public void run(){
-        //          获取数据
-        //          refreshData();
-        //          swiper.setRefreshing(false);
-        //      }
-        //  });
-    }
-    /*private void refreshData(){
-        list.add(0,String.valueOf((int)(Math.random()*10)));
-        adapter.notifyDataSetChanged();
-    }*/
+                //停止刷新
+                mSwipeLayout.setRefreshing(false);
 
+            }
+        }, 3000);
+    }
 }
