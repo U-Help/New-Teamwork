4b0b2e16a8586878dd335730d1ce6085eef37b7f
qth <qth@mail.ustc.edu.cn>
2018-07-29 20:30:34 +0800

    qth

--- app/src/main/java/com/example/fyg/login/HelpGetActivity.java
+++ app/src/main/java/com/example/fyg/login/HelpGetActivity.java
@@ -14,10 +14,25 @@ public class HelpGetActivity extends Activity implements SwipeRefreshLayout.OnRe
     private ListView mListView;
     private ArrayList<String> list = new ArrayList<String>();
     private ArrayAdapter<String> adapter;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_get);
+
+        mListView = (ListView) findViewById(R.id.listview);
+        /**
+         * listview绑定adapter
+         */
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getData());
+        mListView.setAdapter(adapter);
+
+        mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        //绑定刷新时间
+        mSwipeLayout.setOnRefreshListener(this);
+        //设置颜色
+        mSwipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light, android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 }
